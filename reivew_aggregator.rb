6259076fc53a4240edeed826602013bdb41b4f6e
patbrennan <patrick@junecommerce.org>
2016-09-02 09:05:23 +0000

    separate API keys into other file

--- reivew_aggregator.rb
+++ reivew_aggregator.rb
@@ -3,10 +3,16 @@ require "sinatra/reloader" if development?
 require "tilt/erubis"
 require "sinatra/content_for"
 require "yelp"
+require "yaml"
+
+def load_creds(key)
+  creds = YAML.load_file("yelp_creds.yml")
+  creds[key]
+end
 
 configure do
   enable :sessions
-  set :session_secret, "W_3xPj@t^^q{xV!c"
+  set :session_secret, 'W_3xPj@t^^q{xV!c'
   set :erb, :escape_html => true
 end
 
@@ -16,10 +22,10 @@ end
 
 before do
   # is the 'client' var available in every path?
-  client = Yelp::Client.new({ consumer_key: 'jMzBov2lmUMSKPWCvuDxjw',
-                            consumer_secret: 'WDyvycywcvFMdjO1oQBZPcprP28',
-                            token: 'tgfUS0CU3gJ3hYCJJ4TpskANIGO3gEvR',
-                            token_secret: 'agROOLTDm0nRNrmGpUnpnDnJ8hw'
+  client = Yelp::Client.new({ consumer_key: load_creds("CONSUMER_KEY"),
+                            consumer_secret: load_creds("CONSUMER_SECRET"),
+                            token: load_creds("TOKEN"),
+                            token_secret: load_creds("TOKEN_SECRET")
                             })
   
   # use session[:response] to determine whether there are results or not to render?
